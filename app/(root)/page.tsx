5ca178a5a89a0cd8ab2ab24d2ef68dc13b36036b
Carlos Torres <carlos30847012@gmail.com>
2025-04-26 17:31:05 -0400

    set up app, file and folder structure, routing and auth pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Singa Tu madre</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ components/AuthForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { z } from "zod";
+import Link from "next/link";
+import Image from "next/image";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+
+    const router = useRouter();
+
+    const formSchema = authFormSchema(type);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        try {
+            if (type === "sign-up") {
+
+                toast.success('La cuenta ha sido creada correctamente')
+                router.push('/sign-in')
+            } else {
+                toast.success('Inicio de sesión exitoso')
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(`There was an error: ${error}`);
+        }
+    };
+
+    const isSignIn = type === "sign-in";
+
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">PrepÁgil</h2>
+                </div>
+
+                <h3>Entrena para tus entrevistas laborales con un Asistente de Voz</h3>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="w-full space-y-6 mt-4 form"
+                    >
+                        {!isSignIn && (
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                label="Nombre"
+                                placeholder="Tu Nombre"
+                                type="text"
+                            />
+                        )}
+
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            label="E-Mail"
+                            placeholder="Introduce tu E-Mail"
+                            type="email"
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            label="Contraseña"
+                            placeholder="Introduce tu Contraseña"
+                            type="password"
+                        />
+
+                        <Button className="btn" type="submit">
+                            {isSignIn ? "Inicia Sesión" : "Crea una Cuenta"}
+                        </Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    {isSignIn ? "¿Aún no tienes una cuenta?" : "¿Ya tienes una cuenta?"}
+                    <Link
+                        href={!isSignIn ? "/sign-in" : "/sign-up"}
+                        className="font-bold text-user-primary ml-1"
+                    >
+                        {!isSignIn ? "Inicia Sesión" : "Regístrate"}
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default AuthForm;
